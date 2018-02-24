de0da521999d72658abaf468da355179fd36675f
Frank S. Schierle <git@frankschierle.de>
2018-02-24 15:40:53 +0100

    UnitTest class for DocumentationHelper removed.
    DocumentationHelper is tested via analyzers.

--- Schierle.Documentor/Schierle.Documentor.Test/Utils/DocumentationHelperTests/DocumentationHelperTest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Schierle.Documentor.Test.Utils.DocumentationHelperTests
-{
-  using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-  [TestClass]
-  public partial class DocumentationHelperTest
-  {
-  }
-}
